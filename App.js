24d7dfbce04a98c8545cbf8a20115be5a7364c33
samueljbp <samuel_jbp@yahoo.com.br>
2022-10-01 14:17:51 -0300

    Abrindo no mac

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
-import {Text} from 'react-native';
+import {Text, SafeAreaView} from 'react-native';
 
 import {store, persistor} from './src/store';
 
@@ -10,7 +10,9 @@ export default function App() {
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <Text>Hello</Text>
+                <SafeAreaView>
+                    <Text>Hello</Text>
+                </SafeAreaView>
             </PersistGate>
         </Provider>
     );
